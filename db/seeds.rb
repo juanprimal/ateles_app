8e69bb2969bdf33bb99ba0d8dd5e925725eca27c
Pedro Juan Criollo Romo <pedrojuancr@gmail.com>
2014-03-15 16:46:55 -0500

    Added default users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :first_name => 'Admin',:last_name => 'Administrator', :email => 'pedrojuancr@gmail.com', :password => '12345678@', :password_confirmation => '12345678@'
+puts 'New user created: ' << user.first_name
+user2 = User.create! :first_name => 'Test', :last_name => "User", :email => 'test@gmail.com', :password => '12345678@', :password_confirmation => '12345678@'
+puts 'New user created: ' << user2.first_name
